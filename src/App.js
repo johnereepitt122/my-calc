26449f9e41f4ddffc4f5606db1809a8465b8bc12
JohnereePittPangilinan <jpcpangilinan.student@ua.edu.ph>
2023-01-30 15:03:53 +0800

    Third Push - With a little design and Onclick button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 
+
 function Num({ message, children }) {
   return (
     <button onClick={() => alert(message)}>
@@ -20,51 +21,62 @@ function Display () {
 
 
 function App() {
+
+// const[disp, setDisp] = useState('0');
+
+// const numberClickHander = (e) => {
+//   e.preventDefault();
+//   const value = e.targer.innerHTML; 
+//   console.log(value);
+//   alert(value);
+// }
+
   return (
  <div class="whole-container"> 
+ <br></br>
       <div class="heading"> 
+      <br></br>
       <Display></Display>
-      <div class="first"> 
-      
-      <section>
-      <Num message="Playing!"> 7 </Num>
-      <Num message="Uploading!"> 8 </Num>
-      <Num message="Uploading!"> 9 </Num>
-      <Num message="Uploading!"> / </Num>
+     </div>
+
+      <div class="first">  
+      <section class="first-buttons">
+      <Num message="7"> 7 </Num>
+      <Num message="8"> 8 </Num>
+      <Num message="9"> 9 </Num>
+      <Num message="Divided by"> / </Num>
       </section>
       </div>
       
       <div class = "second">
-      <section>
-      <Num message="Playing!"> 4 </Num>
-      <Num message="Uploading!"> 5 </Num>
-      <Num message="Uploading!"> 6 </Num>
-      <Num message="Uploading!"> * </Num>
+      <section class="second-buttons">
+      <Num message="4"> 4 </Num>
+      <Num message="5"> 5 </Num>
+      <Num message="6"> 6 </Num>
+      <Num message="multiply by"> * </Num>
       </section>
       </div>
 
       <div class = "third">
-      <section>
-      <Num message="Playing!"> 1 </Num>
-      <Num message="Uploading!"> 2 </Num>
-      <Num message="Uploading!"> 3 </Num>
-      <Num message="Uploading!"> - </Num>
+      <section class="third-buttons">
+      <Num message="1"> 1 </Num>
+      <Num message="2"> 2 </Num>
+      <Num message="3"> 3 </Num>
+      <Num message="-"> - </Num>
       </section>
       </div>
 
       
       <div class = "fourth">
-      <section>
-      <Num message="Playing!"> CLR </Num>
-      <Num message="Uploading!"> 0 </Num>
-      <Num message="Uploading!"> = </Num>
-      <Num message="Uploading!"> + </Num>
+      <section class ="fourth-buttons">
+      <Num message="clr"> clr </Num>
+      <Num message="0"> 0 </Num>
+      <Num message="="> = </Num>
+      <Num message="+"> + </Num>
       </section>
       </div>
-
       </div>
 
-    </div>
   );
 }
 
