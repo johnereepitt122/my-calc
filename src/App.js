237d2e06e3b7c313b18260b4c94bca5bbd9ef62b
JohnereePittPangilinan <jpcpangilinan.student@ua.edu.ph>
2023-01-30 14:20:39 +0800

    Second Push- The layout of the calcu.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function Num({ message, children }) {
+  return (
+    <button onClick={() => alert(message)}>
+      {children}
+    </button>
+  );
+}
+
+function Display () {
+  return ( 
+    <h1> Display
+    </h1>
+  )
+}
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <div class="whole-container"> 
+      <div class="heading"> 
+      <Display></Display>
+      <div class="first"> 
+      
+      <section>
+      <Num message="Playing!"> 7 </Num>
+      <Num message="Uploading!"> 8 </Num>
+      <Num message="Uploading!"> 9 </Num>
+      <Num message="Uploading!"> / </Num>
+      </section>
+      </div>
+      
+      <div class = "second">
+      <section>
+      <Num message="Playing!"> 4 </Num>
+      <Num message="Uploading!"> 5 </Num>
+      <Num message="Uploading!"> 6 </Num>
+      <Num message="Uploading!"> * </Num>
+      </section>
+      </div>
+
+      <div class = "third">
+      <section>
+      <Num message="Playing!"> 1 </Num>
+      <Num message="Uploading!"> 2 </Num>
+      <Num message="Uploading!"> 3 </Num>
+      <Num message="Uploading!"> - </Num>
+      </section>
+      </div>
+
+      
+      <div class = "fourth">
+      <section>
+      <Num message="Playing!"> CLR </Num>
+      <Num message="Uploading!"> 0 </Num>
+      <Num message="Uploading!"> = </Num>
+      <Num message="Uploading!"> + </Num>
+      </section>
+      </div>
+
+      </div>
+
     </div>
   );
 }
