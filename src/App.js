7539d01499b57be8adc02c5fbf20e161cd8a0628
JohnereePittPangilinan <jpcpangilinan.student@ua.edu.ph>
2023-01-30 16:17:25 +0800

    Fourth Push - Display Number in the designated box when clicked.

--- src/App.js
+++ src/App.js
@@ -1,78 +1,76 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-
-
-function Num({ message, children }) {
+function Num({ message="?", onClick }) {
   return (
-    <button onClick={() => alert(message)}>
-      {children}
+    <button onClick={onClick}>
+      {message}
     </button>
   );
 }
 
-function Display () {
+function Display({display}) {
   return ( 
-    <h1> Display
-    </h1>
-  )
+    <div> {display}
+    </div>
+  );
 }
 
+function App() { 
 
 
-function App() {
+const[disp, setDisp] = useState('0');
 
-// const[disp, setDisp] = useState('0');
-
-// const numberClickHander = (e) => {
-//   e.preventDefault();
-//   const value = e.targer.innerHTML; 
-//   console.log(value);
-//   alert(value);
-// }
+const numberClickHandler = (e) => {
+  e.preventDefault();
+  const value = e.target.innerHTML; 
+  setDisp(value);
+  console.log(value);
+}
 
   return (
  <div class="whole-container"> 
  <br></br>
       <div class="heading"> 
       <br></br>
-      <Display></Display>
-     </div>
+      <Display display= {disp}/> 
+      </div>
 
       <div class="first">  
       <section class="first-buttons">
-      <Num message="7"> 7 </Num>
-      <Num message="8"> 8 </Num>
-      <Num message="9"> 9 </Num>
-      <Num message="Divided by"> / </Num>
+      <Num message="7" onClick={numberClickHandler}/>
+      <Num message="8" onClick={numberClickHandler}/>
+      <Num message="9" onClick={numberClickHandler}/>
+      <Num message="/" onClick={numberClickHandler}/>
       </section>
       </div>
       
       <div class = "second">
       <section class="second-buttons">
-      <Num message="4"> 4 </Num>
-      <Num message="5"> 5 </Num>
-      <Num message="6"> 6 </Num>
-      <Num message="multiply by"> * </Num>
+      <Num message="4" onClick={numberClickHandler}/>
+      <Num message="5" onClick={numberClickHandler}/>
+      <Num message="6" onClick={numberClickHandler}/>
+      <Num message="*" onClick={numberClickHandler}/>
       </section>
       </div>
 
       <div class = "third">
       <section class="third-buttons">
-      <Num message="1"> 1 </Num>
-      <Num message="2"> 2 </Num>
-      <Num message="3"> 3 </Num>
-      <Num message="-"> - </Num>
+      <Num message="1" onClick={numberClickHandler}/>
+      <Num message="2" onClick={numberClickHandler}/> 
+      <Num message="3" onClick={numberClickHandler}/>
+      <Num message="-" onClick={numberClickHandler}/>
       </section>
       </div>
 
       
       <div class = "fourth">
       <section class ="fourth-buttons">
-      <Num message="clr"> clr </Num>
-      <Num message="0"> 0 </Num>
-      <Num message="="> = </Num>
-      <Num message="+"> + </Num>
+      <Num message="clr" onClick={numberClickHandler}/>
+      <Num message="0" onClick={numberClickHandler}/>
+      <Num message="=" onClick={numberClickHandler}/>
+      <Num message="+" onClick={numberClickHandler}/>
       </section>
       </div>
       </div>
